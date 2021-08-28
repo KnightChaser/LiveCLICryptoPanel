47610b0a57c4b2f26d2feeabfabbef3f33589eba
luminosity <agerio100@naver.com>
2021-08-29 01:12:29 +0900

    Colorize for changes
    Add Red/Blue color to highlight changes of crypto values.

--- cryptopanel.py
+++ cryptopanel.py
@@ -4,6 +4,7 @@ import requests
 import json
 import datetime
 from time import sleep
+from colorama import Fore, Back, Style
 
 # gather necessary information
 def gatherInformation():
@@ -36,6 +37,9 @@ if(cryptoVarietyQty > 50 or cryptoVarietyQty < 1):
 # how many times information updated?
 updateCycleCount = 0
 
+# how many times API call failed
+apiCallFailedCount = 0
+
 while True:
 
     cryptoDataBundle, USDKRWExchangeRate = gatherInformation()
@@ -46,6 +50,7 @@ while True:
     if cryptoDataBundle == None or USDKRWExchangeRate == None:
 
         print("API call failed. We'll try after 3 seconds")
+        apiCallFailedCount += 1
         sleep(3)
 
     else:
@@ -54,9 +59,12 @@ while True:
 
         # update metadata
         now = datetime.datetime.now()
-        print("업데이트 시각 : {} | 업데이트 횟수 : {} 회 ".format(now.strftime('%Y년 %m월 %d일 %H시 %M분 %S초'), updateCycleCount), end='\n')
+        print("=== Live CLI Crypto Pannel === ")
+        print("업데이트 시각 : {} | 업데이트 횟수 : {} 회 | API Call 실패 : {} ".format(now.strftime('%Y년 %m월 %d일 %H시 %M분 %S초'), updateCycleCount, apiCallFailedCount))
+        print()             #new line
+
         # process the gathered information
-        print("  암호화폐          현재 가격 (변동량)            24시간 거래량               시가총액                          공급량")
+        print("  암호화폐          현재 가격 (변동량)            24시간 거래량               시가총액                             공급량")
         print("============================================================================================================================================")
 
         for cryptoInfo in cryptoDataBundle:
@@ -71,15 +79,26 @@ while True:
                 marketCapKRW = "{:,}".format(round(float(cryptoInfo.get("marketCapUsd")) * USDKRWExchangeRate))
                 supply = "{:,}".format(round(float(cryptoInfo.get("supply"))))
 
+                # Arrow direction and colorize chagnes rate as change
+                if float(changePercent24Hr) > 0:                                                                            # UP
+                    changePercent24Hr = Fore.RED + str(changePercent24Hr)  + " % " + Style.RESET_ALL
+                    changeDirectionArrow = Fore.RED + "▲" + Style.RESET_ALL
+                elif float(changePercent24Hr) < 0:                                                                          # DOWN
+                    changePercent24Hr = Fore.BLUE + str(changePercent24Hr)  + " % " + Style.RESET_ALL
+                    changeDirectionArrow = Fore.BLUE + "▲" + Style.RESET_ALL
+                else:                                                                                                       # Steady (0% change rate recorded)
+                    changePercent24Hr = Fore.BLACK + str(changePercent24Hr) + " % " + Style.RESET_ALL
+                    changeDirectionArrow = Fore.BLACK + "■" + Style.RESET_ALL
+
                 # maxSupply contents may be None if its value is infinity(∞)
                 if cryptoInfo.get("maxSupply") != None:
                     maxSupply = "{:,}".format(round(float(cryptoInfo.get("maxSupply"))))
                 else:
                     maxSupply = "∞"
 
-                print("""[#{0:0>2}] {1:<6} | ₩ {2:>13} ({3:>7} %) | ₩ {4:>19} | ₩ {5:>22} | {6:>20} / {7:>15} {8: <5}""".
-                    format(rank, symbol, priceKRW, changePercent24Hr, volumeKRW24Hr, marketCapKRW, supply, maxSupply, symbol
+                print("""[#{0:0>2}] {1:<6} | ₩ {2:>13} ({3:>18}{4}) | ₩ {5:>19} | ₩ {6:>22} | {7:>20} / {8:>15} {9: <5}""".
+                    format(rank, symbol, priceKRW, changePercent24Hr, changeDirectionArrow, volumeKRW24Hr, marketCapKRW, supply, maxSupply, symbol
                     ,end=''))
 
         # update interval is every 12 seconds
-        sleep(12)
+        sleep(15)
