1293071c47f41170facaf45c3f461bdda788067a
luminosity <agerio100@naver.com>
2021-08-31 21:35:12 +0900

    Seperate last function into two function to divide their purpose and compare between previous crypto price
    Fixed potential bug like duplicated JSON investigation / divide function into cryptoDataProcessing() and cryptoDataPrinting()

--- LiveCLICryptoPanel_upbitAPI/LiveCryptoPanel_upbitAPI.py
+++ LiveCLICryptoPanel_upbitAPI/LiveCryptoPanel_upbitAPI.py
@@ -53,9 +53,7 @@ def gatherInformation(APIqueryURL):
         return "JSONDecodeErrorAtGatheringInformation"
 
 
-def cryptoDataProcessing(APIqueryURL, cryptoShowQuantity, dataSortCriterion, sortDirection):
-
-    cryptoDataBundle = gatherInformation(APIqueryURL)
+def cryptoDataProcessing(cryptoDataBundle, dataSortCriterion, sortDirection):
 
     # error handling
     if cryptoDataBundle in ["APIqueryURLFailed", "UnexpectedErrorAtGatheringInformation", "JSONDecodeErrorAtGatheringInformation"]:
@@ -64,6 +62,10 @@ def cryptoDataProcessing(APIqueryURL, cryptoShowQuantity, dataSortCriterion, sor
     # how much money have traded in recent 24 hour?
     totalUPBITKRWMarketTradePrice24hr = 0               # KRW Market
     totalUPBITBTCMarketTradePrice24hr = 0               # BTC Market
+
+    # Add extra arrow direction to express whether price went up or down than last update value
+    # store exactly-recent data
+    previousCryptoKRWprice = {"init" : -1}
 
     # for BTC market
     currentBTCprice = float(round(cryptoDataBundle[0]["trade_price"]))          # <-- Its orginal value type was <class='str'>
@@ -74,8 +76,10 @@ def cryptoDataProcessing(APIqueryURL, cryptoShowQuantity, dataSortCriterion, sor
         if "BTC-" in cryptoDataBundle[sequence]["market"]:
             cryptoDataBundle[sequence]["acc_trade_price_24h"] *= currentBTCprice
             totalUPBITBTCMarketTradePrice24hr += cryptoDataBundle[sequence]["acc_trade_price_24h"]
+            previousCryptoKRWprice[cryptoDataBundle[sequence]["market"]] = cryptoDataBundle[sequence]["acc_trade_price_24h"]  # store price data
         else:
             totalUPBITKRWMarketTradePrice24hr += cryptoDataBundle[sequence]["acc_trade_price_24h"]
+            previousCryptoKRWprice[cryptoDataBundle[sequence]["market"]] = cryptoDataBundle[sequence]["acc_trade_price_24h"]  # store price data
 
     # Sort data as criteria with KRW value.
     if sortDirection == 2:              # descending order
@@ -83,8 +87,15 @@ def cryptoDataProcessing(APIqueryURL, cryptoShowQuantity, dataSortCriterion, sor
     else:
         cryptoDataBundle = sorted(cryptoDataBundle, key = lambda x:x[dataSortCriterion], reverse = False)
 
-    try:
+    # return "DataProcessingSuccessful", cryptoDataBundle, previousCryptoKRWprice, totalUPBITKRWMarketTradePrice24hr, totalUPBITBTCMarketTradePrice24hr
+    return "DataProcessingSuccessful", cryptoDataBundle, totalUPBITKRWMarketTradePrice24hr, totalUPBITBTCMarketTradePrice24hr, currentBTCprice
 
+
+def cryptoDataPrinting(cryptoDataBundle, cryptoShowQuantity, totalUPBITKRWMarketTradePrice24hr, 
+                        totalUPBITBTCMarketTradePrice24hr, currentBTCprice):
+
+    try:
+        pass
 
         os.system("cls")
 
@@ -117,8 +128,6 @@ def cryptoDataProcessing(APIqueryURL, cryptoShowQuantity, dataSortCriterion, sor
                 changePrice = "{:,}".format(round(cryptoDataBundle[sequence]["signed_change_price"]))
                 accumulatedTradePrice24hr = "{:,}".format(round(cryptoDataBundle[sequence]["acc_trade_price_24h"]))
                 marketType = "KRW"
-                
-            
 
             if realChangeRate > 0:            # price went up
                 changePrice = Fore.RED + Style.BRIGHT + str(changePrice) + " " + Style.RESET_ALL
@@ -148,7 +157,7 @@ def cryptoDataProcessing(APIqueryURL, cryptoShowQuantity, dataSortCriterion, sor
         return "unexpectedErrorAtCryptoDataProcessing"
 
 
-    return "successfulProcessing"
+    return "everythingWasSuccessful"
 
 
 
@@ -177,13 +186,6 @@ def runProgram():
         sys.exit("Check your selection and try again. It's wrong input. | 선택 입력을 확인하신 후 재시도 해 보세요. 잘못된 입력입니다.")
     os.system("cls")
 
-
-    print("*[1 : 오름차순(ascending)] / [2 : 내림차순(descending)]")
-    sortDirection = int(input("Select order direction. | 정렬 기준을 선택하세요.  : "))
-    if sortDirection not in [1, 2]:
-        sys.exit("Check your selection and try again. It's wrong input. | 선택 입력을 확인하신 후 재시도 해 보세요. 잘못된 입력입니다.")
-    os.system("cls")
-
     supportedSelection = { 1 : "trade_price",
                            2 : "signed_change_price",
                            3 : "signed_change_rate",
@@ -194,8 +196,16 @@ def runProgram():
 
     dataSortCriterion = supportedSelection[dataSortCriterionNumber]
 
+
+    print("*[1 : 오름차순(ascending)] / [2 : 내림차순(descending)]")
+    sortDirection = int(input("Select order direction. | 정렬 기준을 선택하세요.  : "))
+    if sortDirection not in [1, 2]:
+        sys.exit("Check your selection and try again. It's wrong input. | 선택 입력을 확인하신 후 재시도 해 보세요. 잘못된 입력입니다.")
+    os.system("cls")
+
     #######################################################################################################################
 
+    # Initalization essential operation : create API query URL
     APIqueryURL = createQueryURL()
 
     while True:
@@ -203,39 +213,48 @@ def runProgram():
             # os.system("cls")                      # <-- to reduce void screen time, I inserted this into function cryptoDataProcessing()
             now = datetime.datetime.now()
 
-            # run!
-            runtimeResult = cryptoDataProcessing(APIqueryURL, cryptoShowQuantity, dataSortCriterion, sortDirection)
+            # gathering data
+            cryptoDataBundle = gatherInformation(APIqueryURL)
 
-            if apiCallFailedCount == 0 and exceptionCount == 0:
-                uptimeRatio = 100
-            else:
-                uptimeRatio = "{:.2f}".format((updateCycleCount * 100) / (updateCycleCount + apiCallFailedCount + exceptionCount))
-            
-            # runtime log
-            print("=================================================================================================================================================")
-            print("업데이트 시각 : {} | 업데이트 횟수 : {:,} 회 | API Call 실패 : {:,} 회 | 기타 에러 : {:,} 회 | Uptime 비율 : {} % "
-                        .format(now.strftime('%Y년 %m월 %d일 %H시 %M분 %S초'), updateCycleCount, apiCallFailedCount, exceptionCount, uptimeRatio))
-            print("=================================================================================================================================================")
-            print("powered by UPBIT. created by LUMINOUS(blog.naver.com/agerio100 | agerio100@naver.com)")
+            # dataprocessing
+            # Flag, cryptoDataBundle, totalUPBITKRWMarketTradePrice24hr, totalUPBITBTCMarketTradePrice24hr, currentBTCprice = cryptoDataProcessing(APIqueryURL, dataSortCriterion, sortDirection)
+            FLAG, cryptoDataBundle, totalUPBITKRWMarketTradePrice24hr, totalUPBITBTCMarketTradePrice24hr, currentBTCprice = cryptoDataProcessing(cryptoDataBundle, dataSortCriterion, sortDirection)
+
+            # data printing (do only data processing process was done without any error)
+            if FLAG == "DataProcessingSuccessful":
+                FLAG = cryptoDataPrinting(cryptoDataBundle, cryptoShowQuantity, totalUPBITKRWMarketTradePrice24hr, totalUPBITBTCMarketTradePrice24hr, currentBTCprice)
 
             # if everything went successfully
-            if runtimeResult == "successfulProcessing":
+            if FLAG == "everythingWasSuccessful":
                 updateCycleCount += 1
+
+                if apiCallFailedCount == 0 and exceptionCount == 0:
+                    uptimeRatio = 100
+                else:
+                    uptimeRatio = "{:.2f}".format((updateCycleCount * 100) / (updateCycleCount + apiCallFailedCount + exceptionCount))
+                
+                # runtime log
+                print("=================================================================================================================================================")
+                print("업데이트 시각 : {} | 업데이트 횟수 : {:,} 회 | API Call 실패 : {:,} 회 | 기타 에러 : {:,} 회 | Uptime 비율 : {} % "
+                            .format(now.strftime('%Y년 %m월 %d일 %H시 %M분 %S초'), updateCycleCount, apiCallFailedCount, exceptionCount, uptimeRatio))
+                print("=================================================================================================================================================")
+                print("powered by UPBIT. created by LUMINOUS(blog.naver.com/agerio100 | agerio100@naver.com)")
+
                 # wait for designated refresh interval
                 sleep(refreshInterval)
 
             else:
                 # runtime procedure verification
-                if runtimeResult == "APIqueryURLFailed":
+                if FLAG == "APIqueryURLFailed":
                     apiCallFailedCount += 1
                     print("API call creation failed. Retry in 2 seconds. | API 요청 제작 실패. 2초 내 재시도합니다.")
-                elif runtimeResult == "UnexpectedErrorAtGatheringInformation":
+                elif FLAG == "UnexpectedErrorAtGatheringInformation":
                     exceptionCount += 1
                     print("Unexpected error occured at gathering information. Retry in 2 seconds. | 데이터 수집중 예상치 못한 오류 발생. 1초 내 재시도합니다.")
-                elif runtimeResult == "unexpectedErrorAtCryptoDataProcessing":
+                elif FLAG == "unexpectedErrorAtCryptoDataProcessing":
                     exceptionCount += 1
                     print("Unexpected error occured at data processing. Retry in 2 seconds. | 데이터 처리중 예상치 못한 오류 발생. 1초 내 재시도합니다.")
-                elif runtimeResult == "JSONDecodeErrorAtGatheringInformation":
+                elif FLAG == "JSONDecodeErrorAtGatheringInformation":
                     exceptionCount += 1
                     print("JSON Decode exception at data processing. Retry in 2 seconds. | JSON 형태의 데이터를 디코딩하는 중 예외상황발생. 1초 내 재시도합니다.")
                 
